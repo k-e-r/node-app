5cec8516be97ee84d34a7958f0cad1e5178e98d4
k-e-r <damajonboua@gmail.com>
2022-05-01 20:20:35 -0700

    add printing color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,11 @@
-const getNotes = require('./notes.js');
+// const validator = require('validator');
+// const getNotes = require('./notes.js');
+// const chalk = require('chalk');
+import chalk from 'chalk';
 
-const msg = getNotes();
-
-console.log(msg);
+// const msg = getNotes();
+// console.log(msg);
+const Msg = chalk('Success!');
+console.log(Msg);
+const greenMsg = chalk.green.inverse('Success!');
+console.log(greenMsg);
