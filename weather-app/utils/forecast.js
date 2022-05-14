432d5204213894a5bba841d27990186bd1a6fbe8
k-e-r <damajonboua@gmail.com>
2022-05-14 12:32:29 -0700

    add destructuring and property shorthand challenge

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -7,15 +7,15 @@ const forecast = (lat, lon, callback) => {
     ',' +
     lon +
     '&units=f';
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location', undefined);
     } else {
       callback(
         undefined,
-        `It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
+        `It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
       );
     }
   });
