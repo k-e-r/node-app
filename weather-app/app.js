f8a7764ef9a0903a43cdaa4ce5aac21028c61693
k-e-r <damajonboua@gmail.com>
2022-05-14 10:40:30 -0700

    add callback abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const geocode = require('./utils/geocode');
 
 // const url =
 //   'http://api.weatherstack.com/current?access_key=9d9a0af23eff718899fc101ad063adeb&query=New York';
@@ -14,28 +15,21 @@ const request = require('request');
 //   }
 // });
 
-const geocodeURL =
-  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoiay1lcmEiLCJhIjoiY2wyenIzamkxMDJ5ejNjcHJjcDZ3cmdlaiJ9._h2ZHWzfRYmzu5_Zgyrg2w';
-request({ url: geocodeURL, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to weather service!');
-  } else if (response.body.features.length === 0) {
-    console.log('Unable to find location. Try another search.');
-  } else {
-    const lattitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(lattitude, longitude);
-  }
+// const geocodeURL =
+//   'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoiay1lcmEiLCJhIjoiY2wyenIzamkxMDJ5ejNjcHJjcDZ3cmdlaiJ9._h2ZHWzfRYmzu5_Zgyrg2w';
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to weather service!');
+//   } else if (response.body.features.length === 0) {
+//     console.log('Unable to find location. Try another search.');
+//   } else {
+//     const lattitude = response.body.features[0].center[1];
+//     const longitude = response.body.features[0].center[0];
+//     console.log(lattitude, longitude);
+//   }
+// });
+
+geocode('Boston', (error, data) => {
+  console.log('Error', error);
+  console.log('Data', data);
 });
-
-// console.log('Starting');
-
-// setTimeout(() => {
-//   console.log('2 Second Timer');
-// }, 2000);
-
-// setTimeout(() => {
-//   console.log('0 Second Timer');
-// }, 0);
-
-// console.log('Stopping');
