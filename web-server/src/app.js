7d79507683e61fe02fbd1d687a635df51af6dc7a
k-e-r <damajonboua@gmail.com>
2022-05-14 23:23:46 -0700

    add serving up html and json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,29 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-  res.send('Hello express!');
+  res.send('<h1>Hello express!</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('Help page');
+  res.send([
+    {
+      name: 'Andrew',
+    },
+    {
+      name: 'Sarah',
+    },
+  ]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page');
+  res.send({
+    forecast: 'rain',
+    location: 'New York',
+  });
 });
 
 // app.com
