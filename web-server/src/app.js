9517e269767590b3e886e97d420b4b03c4df9b9a
k-e-r <damajonboua@gmail.com>
2022-05-14 23:44:01 -0700

    add serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,16 @@
+const path = require('path');
 const express = require('express');
 
+console.log(__dirname);
+console.log(__filename);
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
   res.send('<h1>Hello express!</h1>');
-});
-
-app.get('/help', (req, res) => {
-  res.send([
-    {
-      name: 'Andrew',
-    },
-    {
-      name: 'Sarah',
-    },
-  ]);
-});
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
