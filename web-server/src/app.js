0798da24ad10997edad626732b2134a19bb5fd02
k-e-r <damajonboua@gmail.com>
2022-05-15 09:25:59 -0700

    add dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,30 @@
 const path = require('path');
 const express = require('express');
 
-console.log(__dirname);
-console.log(__filename);
-
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
-  res.send('<h1>Hello express!</h1>');
+  res.render('index', {
+    title: 'Weather',
+    name: 'Andrew Mead',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Andrew Mead',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'This is some helpful text.',
+  });
 });
 
 app.get('/weather', (req, res) => {
