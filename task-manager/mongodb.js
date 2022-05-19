d415d9b9da65ffdbdc2733b38408abccbdfc458d
k-e-r <damajonboua@gmail.com>
2022-05-19 13:25:37 -0700

    add updating documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,46 +13,39 @@ MongoClient.connect(connectionURL, (error, client) => {
 
   const db = client.db(databaseName);
 
-  // db.collection('users').findOne(
-  //   { _id: new ObjectId('62868145c771cab1afb6a90b') },
-  //   (error, user) => {
-  //     if (error) {
-  //       return console.log('Unable to fetch');
-  //     }
-
-  //     console.log(user);
-  //   }
-  // );
-
   // db.collection('users')
-  //   .find({ age: 27 })
-  //   .toArray((error, users) => {
-  //     console.log(users);
+  //   .updateOne(
+  //     {
+  //       _id: new ObjectId('62868145c771cab1afb6a90b'),
+  //     },
+  //     {
+  //       $inc: {
+  //         age: 1,
+  //       },
+  //     }
+  //   )
+  //   .then((result) => {
+  //     console.log(result);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
   //   });
 
-  // db.collection('users')
-  //   .countDocuments({ age: 27 })
-  //   .then((count) => console.log(count))
-  //   .catch((error) => console.log(error));
-
-  db.collection('tasks').findOne(
-    { _id: new ObjectId('628681ec698a6879a3894e06') },
-    (error, user) => {
-      if (error) {
-        return console.log('Unable to fetch');
-      }
-
-      console.log(user);
-    }
-  );
-
   db.collection('tasks')
-    .find({ completed: false })
-    .toArray((error, tasks) => {
-      if (error) {
-        return console.log('Unable to fetch');
+    .updateMany(
+      {
+        completed: false,
+      },
+      {
+        $set: {
+          completed: true,
+        },
       }
-
-      console.log(tasks);
+    )
+    .then((result) => {
+      console.log(result.modifiedCount);
+    })
+    .catch((error) => {
+      console.log(error);
     });
 });
