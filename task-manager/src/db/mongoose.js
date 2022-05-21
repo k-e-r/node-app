1d716b79589bf7fea650c2a1ed6380e7ed5e2a97
k-e-r <damajonboua@gmail.com>
2022-05-21 11:21:42 -0700

    add resource creation endpoints(part1)

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,62 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const validator = require('validator');
 const connectionURL = process.env.MONGO_URL;
 
 mongoose.connect(connectionURL);
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid');
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Password cannot contain "password"');
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number');
-      }
-    },
-  },
-});
-
-// const me = new User({
-//   name: '      Andrew  ',
-//   email: 'MYEMAIL@MAIL.COM     ',
-//   password: 'phone098!   ',
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log('Error!', error);
-//   });
 
 const Task = mongoose.model('Task', {
   description: {
@@ -69,17 +15,3 @@ const Task = mongoose.model('Task', {
     default: false,
   },
 });
-
-const task = new Task({
-  description: '   Eat lunch',
-  // completed: false,
-});
-
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch((error) => {
-    console.log('Error!', error);
-  });
---
+++ task-manager/src/index.js
@@ -0,0 +1,26 @@
+// require('dotenv').config();
+const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
