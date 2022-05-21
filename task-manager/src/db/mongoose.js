6f7f0febc12b9ffec01791e549910fe504da2b94
k-e-r <damajonboua@gmail.com>
2022-05-21 12:13:36 -0700

    add resource creation endpoints(part2)

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -3,15 +3,3 @@ const mongoose = require('mongoose');
 const connectionURL = process.env.MONGO_URL;
 
 mongoose.connect(connectionURL);
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
