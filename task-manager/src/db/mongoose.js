ffc8ecb6c1c86a9dec63e869cc43db5adce9ee1d
k-e-r <damajonboua@gmail.com>
2022-05-29 19:19:58 -0700

    add promise chaining

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,4 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const connectionURL = process.env.MONGO_URL;
 
-mongoose.connect(connectionURL);
+mongoose.connect(connectionURL, {
+  // useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
